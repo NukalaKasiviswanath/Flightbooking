d8213265a2276b1476a80bf5b5db257d4e7077e0
NUKALA KASI VISWANATH <nukalakasiviswanath@gmail.com>
2023-09-26 01:40:00 -0500

    Initial Commit

---
+++ Flightbooking/FlightBookingVC.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Flightbooking
+//
+//  Created by Kasi Viswanath on 9/26/23.
+//
+
+import UIKit
+
+class FlightBookingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    
+    }
+
+    
+    
+    
+    @IBOutlet weak var TicketConfirmationTV: UITextView!
+    
+    
+    
+    @IBAction func Onewaytrip(_ sender: UIButton) {
+    }
+    @IBOutlet weak var OnewaytripBTN: UIButton!
+    
+    
+    @IBAction func Roundtrip(_ sender: UIButton) {
+    }
+    
+    
+    @IBOutlet weak var RoundtripBTN: UIButton!
+    
+    
+    @IBAction func Bookticket(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var BookticketBTN: UIButton!
+    
+    
+    
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
